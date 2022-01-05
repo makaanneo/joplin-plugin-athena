32019caf4ca7638a2b35efb852f136937dd879f5
Sebastian <233635+makaanneo@users.noreply.github.com>
2022-01-05 09:48:57 +0100

    fix(versioning): enable auto versioning

---
+++ bump-plugin-version.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra');
+
+console.log('process.argv', process.argv);
+
+function bumpVersion(manifestPath, destPath, version) {
+  const contentText = fs.readFileSync(manifestPath, 'utf8');
+  const content = JSON.parse(contentText);
+  content.version = version;
+  fs.writeFileSync(destPath, JSON.stringify(content, null, '\t'), 'utf8');
+}
+
+if (process.argv.length > 2) {
+  bumpVersion(
+    'publish/io.github.makaanneo.Athena.json',
+    'publish/io.github.makaanneo.Athena.json',
+    process.argv[2]
+  );
+}
